1642f2d4d64903f321a44421779e2c0a4ac38dfb
ShuwenHe <1201220707@pku.edu.cn>
2021-03-20 11:57:28 +0800

    feat: Marshal

---
+++ encoding/json/encode.go/Marshal/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Book struct {
+	Title       string
+	Authors     []string
+	Publisher   string
+	IsPublished bool
+	Price       float64
+}
+
+func main() {
+	Marshal()
+}
+
+func Marshal() {
+	gobook := Book{ // Book类型的实例对象
+		"shuwen-os",
+		[]string{"Shuwen", "Richard", "Ritchie"}, // 添加[]string
+		"xstiku.com",
+		true,
+		9.9,
+	}
+	b, err := json.Marshal(gobook) // func Marshal(v interface{}) ([]byte, error) {
+	fmt.Println("b = ", b)         // struct->[]byte
+	fmt.Println("b = ", string(b)) // []byte->string
+	checkErr(err)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "err is %s", err.Error())
+	}
+	return
+}
